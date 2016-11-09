96b21cb703c9303aac1bc899e23428f398408fae
Ben Simms <bsimms.simms@gmail.com>
2016-11-09 14:41:33 +0000

    Update rewriting_aqa_code.py

--- rewriting_aqa_code.py
+++ rewriting_aqa_code.py
@@ -1,8 +1,10 @@
 class pieces(object):
-    piece_dict = {}
-    def piece(func):
+    def __init__(self):
+        self.piece_dict = {}
+        
+    def piece(self, func):
         def predicate():
-            piece_dict[func.__name__] = func
+            self.piece_dict[func.__name__] = func
 
         return predicate
     return func
@@ -27,7 +29,7 @@ class vec2d(object):
                 self.x - vec.x,
                 self.y - vec.y)
         except:
-            raise ValueError("cannot add non type vec2d to vec2d")
+            raise ValueError("cannot sub non type vec2d from vec2d")
 
     def __eq__(self, vec):
         return self.x == vec.x and self.y == vec.y
@@ -51,12 +53,32 @@ class piece(object):
         if self.location == location:
             return self # return self if at location
 
-    def check_self_locations(self, *location):
+    def check_self_locations(self, *locations):
         return not all(map(lambda x: self.location == x, locations))
+
+
 
 class somepiece(piece):
     def __init__(self, x, y, owner):
         super().__init__(x, y, "somepiece", owner)
 
-    def check_move(self, *otherPieces):
-        # +1y -1y +1x -1x if any of own class
+    def validate_move(self, board):
+        
+        
+
+
+class game_board(object):
+    def __init__(self, name :str):
+        self.name = name
+        self.pieces = []
+
+    def set_pieces(self, pieces):
+        self.pieces = pieces
+
+    def add_piece(self, piece):
+        self.pieces.append(piece)
+
+    def check_pieces(self, location):
+        pieces = filter(lambda x: x.check_self_locations(*locations), self.pieces) 
+        
+        
